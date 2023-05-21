e180199b195252e9f0b8afdef4d0b3bd72c6a094
KIHUN KIM <kihunkim@KIHUNui-MacBookAir.local>
2023-05-21 15:44:22 +0900

    Mock pw

--- project/server/src/db/db-client.ts
+++ project/server/src/db/db-client.ts
@@ -8,7 +8,7 @@ export const createDB = async (): Promise<Connection> =>
     port: 3306,
     database: 'ghibli_graphql',
     username: 'root',
-    password: 'rlgns48126',
+    password: '123123',
     logging: !(process.env.NODE_ENV === 'production'),
     synchronize: true, // entities에 명시된 데이터 모델들을 DB에 자동으로 동기화합니다.
     entities: [User], // entities 폴더의 모든 데이터 모델이 위치해야 합니다.
